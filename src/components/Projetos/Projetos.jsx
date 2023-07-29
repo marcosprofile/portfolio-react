baffc7c8c3c2d2a48e903e96f287f0211ce7f9bf
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-29 02:53:06 -0300

    update

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -1,4 +1,5 @@
 import Card from '../Card/Card';
+import Frontend from '../../assets/img/servicos/coder.png';
 import './projetos.css';
 
 export default function Projetos() {
@@ -8,22 +9,27 @@ export default function Projetos() {
       <div className="container__projetos">
         <Card
           nome="Projeto 1"
+          imagem={Frontend}
           descricao="Descrição de demonstração sobre o projeto"
           />
         <Card
           nome="Projeto 2"
+          imagem={Frontend}
           descricao="Descrição de demonstração sobre o projeto"
           />
         <Card
           nome="Projeto 3"
+          imagem={Frontend}
           descricao="Descrição de demonstração sobre o projeto"
           />
         <Card
           nome="Projeto 4"
+          imagem={Frontend}
           descricao="Descrição de demonstração sobre o projeto"
           />
         <Card
           nome="Projeto 5"
+          imagem={Frontend}
           descricao="Descrição de demonstração sobre o projeto"
           />
       </div>
